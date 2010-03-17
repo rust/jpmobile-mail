9d61a4c86aca60a572f231e9d1d2a3ed548ebf7e
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-03-17 18:50:19 +0900

    Update PC conversion table
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- lib/jpmobile/emoticon/z_combine.rb
+++ lib/jpmobile/emoticon/z_combine.rb
@@ -21,9 +21,11 @@ module Jpmobile
     EMOTICON_UNICODES = UNICODE_TO_SJIS.keys|SOFTBANK_UNICODE_TO_WEBCODE.keys.map{|k|k+0x1000}
     UTF8_REGEXP = Regexp.union(*EMOTICON_UNICODES.map{|x| [x].pack('U')}).freeze
 
-    # for PC convertion "GETA"
-    CONVERSION_TABLE_TO_PC = {}
-    CONVERSION_TABLE_TO_SOFTBANK.each{|k, v| CONVERSION_TABLE_TO_PC[k] = 0x3013}
+    # for PC conversion "GETA"
+    CONVERSION_TABLE_TO_PC = Hash[*(CONVERSION_TABLE_TO_SOFTBANK.keys|CONVERSION_TABLE_TO_DOCOMO.keys|CONVERSION_TABLE_TO_AU.keys).map{|k| [k, 0x3013]}.flatten]
+    # CONVERSION_TABLE_TO_SOFTBANK.each{|k, v| CONVERSION_TABLE_TO_PC[k] = 0x3013}
+    # CONVERSION_TABLE_TO_DOCOMO.each{|k, v| CONVERSION_TABLE_TO_PC[k] = 0x3013}
+    # CONVERSION_TABLE_TO_AU.each{|k, v| CONVERSION_TABLE_TO_PC[k] = 0x3013}
 
     SOFTBANK_SJIS_REGEXP = Regexp.union(*SOFTBANK_SJIS_TO_UNICODE.keys.map{|s| Regexp.compile(Regexp.escape([s].pack('n'),"s"),nil,'s')}).freeze
 
